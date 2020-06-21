56f74d7d5cd1abe048c430c7aef951633639f31a
franknoh <42113651+franknoh@users.noreply.github.com>
2020-06-21 18:48:49 +0900

    Create coder.js

---
+++ scripts/debug/coder.js
@@ -0,0 +1,17 @@
+const replier = {};
+const Api = {};
+
+//replier.reply(String msg); //msg를 전송
+replier.reply = function(msg){
+}
+
+//replier.reply(String room, String msg, Boolean hideToast = false); //room에 msg를 전송. 전송 성공 시 true, 실패 시 false를 반환
+replier.reply = function(room, msg, hideToast){
+}
+
+//Api.replyRoom(String room, String msg, Boolean hideToast = false); //room에 msg를 전송. 전송 성공 시 true, 실패 시 false를 반환.
+Api.replyRoom = function(String room, String msg, Boolean hideToast = false){
+}
+
+//레거시 api에 있는거 다 해야됨
+//귀찬타 오늘은 여기까지만
