cb032df79040a3f63c4b0dc3776e4f1983ffc2c5
franknoh <42113651+franknoh@users.noreply.github.com>
2020-06-28 12:49:02 +0900

    Update API.js

--- scripts/API.js
+++ scripts/API.js
@@ -1,17 +1,10 @@
-const replier = {};
 const Api = {};
-
-//replier.reply(String msg); //msg를 전송
-replier.reply = function(msg){
-}
-
-//replier.reply(String room, String msg, Boolean hideToast = false); //room에 msg를 전송. 전송 성공 시 true, 실패 시 false를 반환
-replier.reply = function(room, msg, hideToast){
-}
-
-//Api.replyRoom(String room, String msg, Boolean hideToast = false); //room에 msg를 전송. 전송 성공 시 true, 실패 시 false를 반환.
-Api.replyRoom = function(String room, String msg, Boolean hideToast = false){
-}
-
-//레거시 api에 있는거 다 해야됨
-//귀찬타 오늘은 여기까지만
+const Utils = {};
+const FileStream = {};
+const DataBase = {};
+const Log = {};
+const Device = {};
+const Bridge = {};
+const AppData = {};
+const replier = {};
+const imageDB = {};
