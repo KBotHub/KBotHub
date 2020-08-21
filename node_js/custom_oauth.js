430747392f9d7744d83749c23a7b29fede5f389e
franknoh <42113651+franknoh@users.noreply.github.com>
2020-08-21 23:57:03 +0900

    Update custom_oauth.js

--- node_js/custom_oauth.js
+++ node_js/custom_oauth.js
@@ -1,6 +1,6 @@
 var admin = require("firebase-admin");
 
-var serviceAccount = require("https://firebasestorage.googleapis.com/v0/b/editor-280309.appspot.com/o/editor-280309-firebase-adminsdk-f7jde-e813e696fb.json?alt=media&token=be3f835f-6e88-43ca-b3d7-38df3927d1c1");
+var serviceAccount = require("${{secrets.OAUTH_NAVER}}");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
